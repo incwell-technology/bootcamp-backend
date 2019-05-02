6d15b0a4669d83b0b20bd0f75a273ea9530f769e
saijal <saijalshakya@gmail.com>
2019-05-02 11:26:27 +0545

    bug: api prefix solved

--- api/urls.py
+++ api/urls.py
@@ -4,11 +4,11 @@ from visitor import api as visitor_api
 from bootcamp.api import CompanyViewSet, MentorViewSet, ContentViewSet
 
 router = routers.DefaultRouter()
-router.register('api/courses', CourseViewSet, 'courses')
-router.register('api/students', visitor_api.StudentViewSet, 'students')
-router.register('api/company', CompanyViewSet, 'company')
-router.register('api/mentors', MentorViewSet, 'mentors')
-router.register('api/contents', ContentViewSet, 'contents')
-router.register('api/talk-to-advisor', visitor_api.TalkViewSet, 'talk')
+router.register('courses', CourseViewSet, 'courses')
+router.register('students', visitor_api.StudentViewSet, 'students')
+router.register('company', CompanyViewSet, 'company')
+router.register('mentors', MentorViewSet, 'mentors')
+router.register('contents', ContentViewSet, 'contents')
+router.register('talk-to-advisor', visitor_api.TalkViewSet, 'talk')
 
 urlpatterns = router.urls
